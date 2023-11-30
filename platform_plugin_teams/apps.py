193b22013267d4aee7672942e070efb2d4f9d9cb
Bryann Valderrama <bryanttandrei2015@gmail.com>
2023-11-30 12:07:17 -0500

    feat: make plugin setup

--- platform_plugin_teams/apps.py
+++ platform_plugin_teams/apps.py
@@ -11,3 +11,21 @@ class PlatformPluginTeamsConfig(AppConfig):
     """
 
     name = "platform_plugin_teams"
+    verbose_name = "Custom Teams API Plugin"
+
+    plugin_app = {
+        "url_config": {
+            "lms.djangoapp": {
+                "namespace": "custom-teams",
+                "regex": r"^custom-teams/",
+                "relative_path": "urls",
+            },
+        },
+        "settings_config": {
+            "lms.djangoapp": {
+                "test": {"relative_path": "settings.test"},
+                "common": {"relative_path": "settings.common"},
+                "production": {"relative_path": "settings.production"},
+            },
+        },
+    }
