86204e04dded5764b3968d0343f0b81b8a281e8b
Bryann Valderrama <bryanttandrei2015@gmail.com>
2023-11-30 12:07:49 -0500

    feat: add settings files

---
+++ platform_plugin_teams/settings/common.py
@@ -0,0 +1,10 @@
+"""
+Common settings for the plugin.
+"""
+
+
+def plugin_settings(_):
+    """
+    Set of plugin settings used by the Open Edx platform.
+    More info: https://github.com/edx/edx-platform/blob/master/openedx/core/djangoapps/plugins/README.rst
+    """
---
+++ platform_plugin_teams/settings/production.py
@@ -0,0 +1,10 @@
+"""
+Production settings for the plugin.
+"""
+
+
+def plugin_settings(_):
+    """
+    Set of plugin settings used by the Open Edx platform.
+    More info: https://github.com/edx/edx-platform/blob/master/openedx/core/djangoapps/plugins/README.rst
+    """
